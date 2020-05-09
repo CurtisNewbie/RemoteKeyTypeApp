7f26c0e6962d3691959a160576c2bf74f85ba24b
CurtisNewbie <zhuangyongj@gmail.com>
2020-05-09 13:45:23 +0800

    Impl util class to generate random string

---
+++ quarkus-back/src/main/java/com/curtisnewbie/crypto/RandomGenerator.java
@@ -0,0 +1,35 @@
+package com.curtisnewbie.crypto;
+
+import java.security.SecureRandom;
+
+import javax.enterprise.context.Dependent;
+
+/**
+ * ------------------------------------
+ * <p>
+ * Author: Yongjie Zhuang
+ * <p>
+ * ------------------------------------
+ * <p>
+ * Util class the generates random string
+ * </p>
+ */
+@Dependent
+public class RandomGenerator {
+
+    public final String CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    protected SecureRandom rand = new SecureRandom();
+
+    /**
+     * Generate random string with the specified length
+     * 
+     * @param len length of the generated random string
+     */
+    public String randomStr(int len) {
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            sb.append(CHARS.charAt(rand.nextInt(CHARS.length())));
+        }
+        return sb.toString();
+    }
+}
